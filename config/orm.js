89f9400e7597a9d8d9cfc83c775a221019ccf8b5
Timothy Bedsole <timbedsole@gmail.com>
2018-08-26 18:35:29 -0400

    added logic to orm.js

--- config/orm.js
+++ config/orm.js
@@ -4,16 +4,32 @@ var connection = require('./connections.js');
 
 //creating the ORM and the methods to export
 var orm = {
-    selectAll: function(){
-
+    selectAll: function(cb){
+        var queryString = 'SELECT * FROM burgers';
+        connection.query(queryString, function(err, result){
+            if (err) throw err;
+            cb(result);
+        })
     },
 
-    insertOne: function(){
-
+    insertOne: function(insertVal, cb){
+        var queryString = 'INSERT INTO burgers (burger_name) ';
+        queryString += 'VALUES (' + insertVal + ')';
+        console.log(queryString);
+        connection.query(queryString, function(err, result){
+            if (err) throw err;
+            cb(result);
+        })
     },
 
-    updateOne: function(){
-
+    updateOne: function(updateVal, oldVal, cb){
+        var queryString = 'UPDATE burgers SET burger_name=';
+        queryString += updateval + ' WHERE burger_name=' +  oldVal;
+        console.log(queryString);
+        connection.query(queryString, function(err, result){
+            if (err) throw err;
+            cb(result);
+        })
     }
 };
 
