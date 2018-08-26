a43b51f27b739684561bdff68613ae27b0d5cf68
Timothy Bedsole <timbedsole@gmail.com>
2018-08-26 18:56:46 -0400

    added methods to burger.js

--- models/burger.js
+++ models/burger.js
@@ -1,8 +1,24 @@
 //Requiring the orm object so we can utilize the methods of it.
 var orm = require('../config/orm.js');
 
+//Methods that utilize the orm to query the DB
 var burger = {
-
+    //Console logs all items in the burgers table
+    getAll: function(cb){
+        orm.selectAll('burgers', function(res){
+            cb(res);
+        })
+    },
+    insertOne: function(testVal, cb){
+        orm.insertOne(testVal, function(res){
+            cb(res);
+        })
+    },
+    updateOne: function(updateVal, oldVal, cb){
+        orm.updateOne(updateVal, oldVal, function(res){
+            cb(res);
+        })
+    }
 };
 
 module.exports = burger;
