268c74445166c5079cee106b999ec7e7567624af
Timothy Bedsole <timbedsole@gmail.com>
2018-08-27 14:51:50 -0400

    Added partial to views with burgerBlock file in handlebars to hold the data dash of the devoured value after click

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -2,7 +2,12 @@ $(function(){
     $('.eatBurger').on('click', function(event){
         var id = $(this).data('id');
         var isDevoured = $(this).data('devoured');
-        console.log(id);
-        console.log(isDevoured);
+        // console.log(id);
+        // console.log(isDevoured);
+
+        $.ajax('api/burgers/' + id, {
+            type: 'PUT',
+            data: isDevoured
+        })
     })
 })
