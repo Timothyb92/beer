fd5954a577b907f8f57d8bc4e109c4861255d1c5
Timothy Bedsole <timbedsole@gmail.com>
2018-08-26 20:27:09 -0400

    added post and put methods to controller

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -9,7 +9,26 @@ router.get('/',function(req, res){
         };
         console.log(hbarsObj);
     })
+});
+
+router.post('/api/burgers', function(req, res){
+    burger.insertOne('burger_name', req.body.name, function(result){
+        res.json({ id: result.insertId });
+    });
 })
+
+router.put('api/burgers/:id', function(req, res){
+    var condition = 'id = ' + req.params.burger;
+    burger.updateOne({
+        burger_name: req.body.burger_name
+    }, condition, function(result){
+        if (result.changedRows == 0){
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
 
 
 module.exports = router;
