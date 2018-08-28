33e13212af3011df151fb67d575ad46c38eb566c
Timothy Bedsole <timbedsole@gmail.com>
2018-08-28 09:33:34 -0400

    Added jawsdb credentials to connection.js

--- config/connections.js
+++ config/connections.js
@@ -1,19 +1,24 @@
 //Setting up connection to db
 var mysql = require('mysql');
+var connection;
 
-var connection = mysql.createConnection({
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'root',
     database: 'burgers_db',
-    port: process.env.PORT || 8889
-})
+    })
+}
+
 
 //Making the connection to db
 connection.connect(function(err){
     if (err) throw err;
     console.log('Connected as id ' + connection.threadId);
-})
+});
 
 
 //Exporting connection to db for use in other files
